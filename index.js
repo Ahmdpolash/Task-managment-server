a651e593e251085c855b6572cdfa287eda0025d4
Polash <ahmedpolash732@gmail.com>
2023-12-22 07:20:28 -0600

    completed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yrssrk8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,6 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const usersCollection = client.db("Task_Management").collection("users");
+    const tasksCollection = client.db("Task_Management").collection("tasks");
 
     //! users
     app.post("/users", async (req, res) => {
@@ -41,6 +42,57 @@ async function run() {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+
+    //! task
+
+    app.post("/tasks", async (req, res) => {
+      const tasks = req.body;
+      const result = await tasksCollection.insertOne(tasks);
+      res.send(result);
+    });
+
+    app.get("/tasks", async (req, res) => {
+      const result = await tasksCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const tasks = req.params.id;
+      console.log(tasks);
+      const query = { _id: new ObjectId(tasks) };
+      const result = await tasksCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //update
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+
+      const tasks = req.body;
+      console.log(tasks);
+      const query = { _id: new ObjectId(id) };
+
+
+      const updateDoc = {
+        $set: {
+          title: tasks.title,
+          priority: tasks.priority,
+          deadline: tasks.deadline,
+          description: tasks.description,
+        },
+      };
+      const result = await tasksCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+
+    app.get('/tasks/singleTask/:id', async (req, res) => {
+      const id = req.params.id ;
+      const query = {_id :new ObjectId(id)}
+      const result = await tasksCollection.findOne(query)
+      res.send(result)
+    })
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
